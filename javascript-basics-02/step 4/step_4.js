c7a261fbc70d3373ece54b0eb9a21e251e3d1a70
Layla Hammoud <laylahammoud63@gmail.com>
2023-08-31 22:20:51 +0300

    finish the fourth step completely

--- javascript-basics-02/step 4/step_4.js
+++ javascript-basics-02/step 4/step_4.js
@@ -1,9 +1,10 @@
 let inputs = document.getElementsByTagName('input')
 let reset = document.querySelector("button")
-reset.addEventListener("click",()=>{
-    console.log(inputs.name)
-    inputs.name.value="";
-    inputs.surname.value="";
-    inputs.city.value="";
+reset.addEventListener("click", () => {
+    if (confirm("Are you sure you want to clear the inputs?")) {
+        inputs.name.value = "";
+        inputs.surname.value = "";
+        inputs.city.value = "";
+    };
 
 })
