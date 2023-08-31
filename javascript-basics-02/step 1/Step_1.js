14842823ea7cfcaa8bd9ac9371f1ff646141e695
Layla Hammoud <laylahammoud63@gmail.com>
2023-08-31 23:07:14 +0300

    fix the first step from a bug

--- javascript-basics-02/step 1/Step_1.js
+++ javascript-basics-02/step 1/Step_1.js
@@ -1,4 +1,4 @@
 const img = document.querySelector("#image1")
 img.addEventListener('mouseover',function(){
-    img.setAttribute=("src","images/image1_2.jpg")
+    img.src="images/image1_2.jpg"
 })
